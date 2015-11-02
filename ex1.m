b4ade7e00e26513f4b912e0d69fef0ab11c246d3
Quentin VALLIN <qvallin@pc-info-193.priv.enst-bretagne.fr>
2015-11-02 21:04:46 +0100

    ok

--- ex1.m
+++ ex1.m
@@ -9,7 +9,7 @@ fp = (1/(pi*lambda))*sqrt((-log(p))/(2));
 
 delta_t = 20*tp;
 
-t=0:1/F:delta_t;
+ t=0:1/F:delta_t;
 
 s_t = (1/(lambda*sqrt(2*pi)))*exp(-1*((t-tp).^2/(2*lambda^2)));
 
@@ -42,12 +42,21 @@ title('Signal u(t)');
 fft_u = fft(u_t);
 f=-F/2:1/delta_t:F/2;
 
+length(fft_u)
+
 figure(2);
 subplot(2,1,1);
+% 
+% plot(f,abs(fft_u));
+% subplot(2,1,2);
+% plot(f, angle(fft_u));
 
-plot(f,abs(fft_u));
+fft_shift = fftshift(fft_u);
+
+plot(f,abs(fft_shift));
 subplot(2,1,2);
-plot(f, angle(fft_u));
+plot(f, angle(fft_shift));
+
 
 figure(3);
 fft_shift = fftshift(fft_u);
@@ -65,13 +74,36 @@ plot(f(milieu-delta:milieu+delta),angle(fft_shift(milieu-delta:milieu+delta)));
 
 w=1;
 beta = 0.8;
-val = (1-beta)/(2*w);
+valneg = (1-beta)/(2*w);
+valpos = (1+beta)/(2*w);
 
-f_filtre = -fp:fp;
-h_f = sqrt(w).*(abs(f_filtre)<=(val)) + sqrt((w/2)*(1+sin(pi*w/beta*((1/(2*w))-abs(f_filtre))))).*(abs(f_filtre) >= val & abs(f_filtre) <= val);
+f_filtre = -F/2:1/delta_t:F/2;
+h_f = sqrt(w).*(abs(f_filtre)<=(valneg)) + sqrt((w/2)*(1+sin(pi*w/beta*((1/(2*w))-abs(f_filtre))))).*(abs(f_filtre) >= valneg & abs(f_filtre) <= valpos);
 
 figure(4);
 plot(f_filtre, abs(h_f));
 
+
 figure(5);
-plot(ifft(h_f));
+f_filtre = -fp:1/delta_t:fp;
+h_f_zoom = sqrt(w).*(abs(f_filtre)<=(valneg)) + sqrt((w/2)*(1+sin(pi*w/beta*((1/(2*w))-abs(f_filtre))))).*(abs(f_filtre) >= valneg & abs(f_filtre) <= valpos);
+
+plot(f_filtre, abs(h_f_zoom));
+
+%%%%%%%%%%%% RCS %%%%%%%
+
+figure(6);
+
+h_t = fftshift(real(ifft(h_f)));
+plot(t,abs(h_t));
+
+%%%%%%%%%% filtrage %%%%%%
+
+figure(7);
+
+x_f = h_f .* fft_shift;
+x_t = ifft(2*x_f);
+
+plot(t, abs(x_t));
+hold on;
+plot(t, u_t, 'r');
